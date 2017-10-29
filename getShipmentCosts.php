7ba75d9424b6572a5ee300d062e6cee953a3f53c
M8te <32654880+M8te@users.noreply.github.com>
2017-10-29 18:32:27 +1100

    Add files via upload

--- getShipmentCosts.php
+++ getShipmentCosts.php
@@ -26,45 +26,56 @@ class CalculateShipment {
 		
 	}
 	
-	function determinecosts($frompostcode, $topostcode, $lenghtCM, $widthCM, $heighCM, $weightKG, $serviceType, $countrycode) 
+	function determinecosts($frompostcode, $topostcode, $lenghtCM, $widthCM, $heighCM, $weightKG, $servicetype, $countrycode) 
 	{
+		
+		/*
+		 * Function returns shipment costs for domestic or international shipments
+		 * Parameters:
+		 * $frompostcode = postcode from where item will ship (mandatory for domestic shipment)
+		 * $topostcode = postcode to where item will ship  (mandatory for domestic shipment)
+		 * $lenghtCM = length of item to be shipped in centimeters  (mandatory for domestic shipment)
+		 * $widthCM = width of item to be shipped in centimeters  (mandatory for domestic shipment)
+		 * $heightCM = height of item to be shipped in centimeters  (mandatory for domestic shipment)
+		 * $weightKG = weight of item to be shipped in Kilograms (mandatory for domestic shipment)
+		 * $serviceType = domestic or international type of service / delivery (mandatory for domestic and international shipment)
+		 * $countrycode = country to which item will be shipped (mandatory ONLY for international shipment)
+		 * 
+		 */
 		
 		global $debug;
 		global $shipmentConfig;
 		
+		// build URL query parameters
 		$queryParameters = array(
 				"weight" => $weightKG,
-				"service_code" => $serviceType);
+				"service_code" => $servicetype);
 		
+		// parameters for domestic shipments only 
 		if(!$countrycode){
 			$queryParameters['from_postcode'] = $frompostcode;
 			$queryParameters['to_postcode'] = $topostcode;
 			$queryParameters['length'] = $lenghtCM;
 			$queryParameters['width'] = $widthCM;
 			$queryParameters['height'] = $heighCM;
+			
+			//set domestic endpoint
 			$this->endpoint = $shipmentConfig['domesticendpoint'];
 		}
 		else 
 		{
+			//parameters for international shipments only
 			$queryParameters['country_code'] = $countrycode;
+			
+			//set international end point
 			$this->endpoint = $shipmentConfig['internationalendpoint'];
 		}
 			
 	
-	if($debug)
-		print_r($queryParameters);
+		if($debug)
+			print_r($queryParameters);
 		
-/*		
-		$queryParameters = array(
-				"from_postcode" => $frompostcode,
-				"to_postcode" => $topostcode,
-				"length" => $lenghtCM,
-				"width" => $widthCM,
-				"height" => $heighCM,
-				"weight" => $weightKG,
-				"service_code" => $serviceType
-		);
-*/	
+		// build URL + query string
 		$calculateRateURL = $this->host . $this->endpoint . http_build_query($queryParameters);
 		
 		if($debug)
@@ -73,7 +84,7 @@ class CalculateShipment {
 		//initalize curl
 		$ch = curl_init();
 				
-		//Calculate the final domestic parcel delivery price
+		//Calculate the delivery cost
 		curl_setopt($ch, CURLOPT_URL, $calculateRateURL);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('AUTH-KEY: ' . $this->apikey));
@@ -86,28 +97,31 @@ class CalculateShipment {
 			die('An error has occurred: "' . curl_error($ch) . '" with code: ' . curl_errno($ch));
 		}
 		
-		// parse json to array
+		// decode json into array
 		$jsonresponse = json_decode($rawBody, true);
 		
 		if($debug)
-			//print_r($jsonresponse);
+			print_r($jsonresponse);
 		
-			if(isset($jsonresponse['postage_result']['total_cost']))
-				$this->totalshipmentcost = $jsonresponse['postage_result']['total_cost'];
-			
-			if(isset($jsonresponse['postage_result']['delivery_time']))
-				$this->deliverytimemessage = $jsonresponse['postage_result']['delivery_time'];
-			//echo "TEst:" . $jsonresponse['postage_result']['total_cost'];
+		// set total cost if found in json response
+		if(isset($jsonresponse['postage_result']['total_cost']))
+			$this->totalshipmentcost = $jsonresponse['postage_result']['total_cost'];
+		
+		// set delivery time if found in json response.
+		if(isset($jsonresponse['postage_result']['delivery_time']))
+			$this->deliverytimemessage = $jsonresponse['postage_result']['delivery_time'];
 		
 		
 		return true;
 	}
 	
 	function getDeliveryTimeMessage(){
+		//return parcel delivery time message
 		return $this->deliverytimemessage;
 	}
 	
 	function getItemShipmentCost(){
+		//return item shipment cost
 		return $this->totalshipmentcost;
 	}
 }
