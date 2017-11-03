b137c95a77c13e6802abdde716ecf6a024e0a783
mq-44542291 <32979286+mq-44542291@users.noreply.github.com>
2017-11-03 20:49:18 +1100

    Add files via upload

--- getAddress.php
+++ getAddress.php
@@ -3,11 +3,9 @@
 require_once "./common_db.php";
 
 
-function queryAllAddress($shopper){
+function queryAllAddress($shopper_pk){
 # Queries the database for shipping details for provided user and returns these as a PDO
 	
-	// Create a query to retrieve the users address details
-	//$query = 'select * from shaddr';
 	$query = 'select';
     $query .= ' shaddr_id as "Address_ID",';
     $query .= ' shopper_id as "Shopper_Id",';
@@ -21,52 +19,65 @@ function queryAllAddress($shopper){
     $query .= ' sh_postcode as "Postcode",';
     $query .= ' sh_country as "Country"';
 	$query .= ' from shaddr';
-	$query .= ' where shopper_id = ' . $shopper;
-
+	$query .= ' where shopper_id = ?';
 	
-	$queryOutput = run_db_query($query); // Run query on DB and store result
-	return $queryOutput;// Return query results
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($shopper_pk));
+	
+	return $statement;// Return query results
 }
 
 
 function queryDefaultAddress($shopper_pk){
 # Returns the pk of the default shipping address for the supplied user	
 
-	$query = 'select sh_primaryaddressfk from shopper where shopper_id = ' . $shopper_pk; // SQL query
-	$queryOutput = run_db_query($query); // Run query on DB and store result
-	$row  = $queryOutput -> fetch(); // Get the first value, as only one row returned by query
+	$query = 'select sh_primaryaddressfk from shopper where shopper_id = ?';
 	
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($shopper_pk));
+	
+	$row  = $statement -> fetch(); // Get the first value, as only one row returned by query
 	return $row[0]; // Return the first value as only one value returned by query
 }
 
-function queryOrderAddress($order){
+
+function queryOrderAddress($order_pk){
 # Returns the address fk for the address listed for this order
 
-	$query = 'select order_shaddr from "Order" where order_id = ' . $order; // SQL query
-	$queryOutput = run_db_query($query); // Run query on DB and store result
-	$row  = $queryOutput -> fetch(); // Get the first value, as only one row returned by query
+	$query = 'select order_shaddr from "Order" where order_id = ?';
+	
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($order_pk));
 
+	$row  = $statement -> fetch(); // Get the first value, as only one row returned by query
 	return $row[0]; // Return the first value as only one value returned by query
 }
 
 
 function queryAdressPKs($shopper_pk){
-# Returns an array of the addresses PKs a user hash
+# Returns an array of the addresses PKs a user has
+# This is used for cross checking what valid addresses a user has, therefore only the address_pk is required
 
-		$query = 'select shaddr_id from shaddr where shopper_id = ' . $shopper_pk;
-		$queryOutput = run_db_query($query); // Run query on DB and store result
+		$query = 'select shaddr_id from shaddr where shopper_id = ?';
+
+		$dbo = db_connect(); // Connect to DB and run query
+		$statement = $dbo->prepare($query);
+		$statement->execute(array($shopper_pk)); 
+
 		$outputArray = array();
-	
-		while ($row_addresses = $queryOutput->fetch()){
-			array_push($outputArray, $row_addresses[0]);
+		while ($row_addresses = $statement->fetch()){
+			array_push($outputArray, $row_addresses[0]); // Add just the address_pk to the array
 		}	
 	
 		return $outputArray;
 }
 
+
 function querySpecificAddress($address_pk){
-	
-//	echo($address_pk);
+# Returns an array relating to the address values for the specified address_pk
 	
 	$query = 'select';
     $query .= ' shaddr_id as "Address_ID",';
@@ -81,41 +92,59 @@ function querySpecificAddress($address_pk){
     $query .= ' sh_postcode as "Postcode",';
     $query .= ' sh_country as "Country"';
 	$query .= ' from shaddr';
-	$query .= ' where shaddr_id = ' . $address_pk;
+	$query .= ' where shaddr_id = ?';
 	
-	$queryOutput = run_db_query($query); // Run query on DB and store result
-	$row  = $queryOutput -> fetch(); // Get the first value, as only one row returned by query
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($address_pk));
 
-	return $row; // Return the first value as only one value returned by query
-	
+	return $statement -> fetch(); // Return the first value as only one value returned by query
 }
 
 
-function updateOrderAddress($address, $order){
+function updateOrderAddress($address_pk, $order_pk){
+# Updates the address value in a given order to the provided address_pk
 
-	$query = 'update "Order" set order_shaddr = ' . $address . ' where order_id = ' . $order;
-	run_db_query($query);
+	$query = 'update "Order" set order_shaddr = ? where order_id = ?';
+	
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($address_pk, $order_pk));
 }
+
 
 function queryOrderShippingType($order_pk){
 # Returns the shipping method set in the order
 
-		$query = 'select order_shipping_type from "Order" where order_id = ' . $order_pk;
-		$queryOutput = run_db_query($query); // Run query on DB and store result
-		$row  = $queryOutput -> fetch(); // Get the first value, as only one row returned by query	
-		return $row;
+	$query = 'select order_shipping_type from "Order" where order_id = ?';
+	
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($order_pk));
+		
+	return  $statement -> fetch(); // Return the first value, as only one row returned by query	
 }
 
-function updateOrderShippingType($shipping_type, $order){
 
-	$query = 'update "Order" set order_shipping_type = \'' . $shipping_type . '\' where order_id = ' . $order;
-	run_db_query($query);
+function updateOrderShippingType($shipping_type, $order_pk){
+
+	$query = 'update "Order" set order_shipping_type = ? where order_id = ?';
+	
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($shipping_type, $order_pk));
 }
+
 
 function queryEmail($shopper_pk){
-	$query = 'select sh_email from shopper where shopper_id = ' . $shopper_pk; 
-	$queryOutput = run_db_query($query); // Run query on DB and store result
-	$row  = $queryOutput -> fetch(); // Get the first value, as only one row returned by query	
+	
+	$query = 'select sh_email from shopper where shopper_id = ?'; 
+	
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($shopper_pk));
+	
+	$row  = $statement -> fetch(); // Get the first value, as only one row returned by query	
 	return $row[0]; // Return the first value as only single string in output
 }
 
--- getCart.php
+++ getCart.php
@@ -6,7 +6,6 @@ require_once "./common_db.php";
 function queryOrder($order_pk){
 # Returns all the items on the order for the provided order_pk
 	
-	# Build the SQL query
 	$query = 'select';
 	$query .= ' product.prod_name as "product",';
 	$query .= ' attribute.name || \':\' || attributevalue.attrval_value as "type",';
@@ -24,17 +23,19 @@ function queryOrder($order_pk){
 	$query .= ' on orderproductattributevalues.opattr_attr_id = attribute.id';
 	$query .= ' inner join attributevalue';
 	$query .= ' on orderproductattributevalues.opattr_attrval_id = attributevalue.attrval_id';
-	$query .= ' where "Order".order_id = ' . $order_pk;
+	$query .= ' where "Order".order_id = ?';
 	
-	$queryOutput = run_db_query($query); // Run query on DB and store result
-	return $queryOutput;// Return query results
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($order_pk));
 	
+	return $statement;// Return query results
 }
+
 
 function getSubtotal($order_pk){
 # Returns the total cost of all the items on the order	
 
-	// Build query to get all items/quantities/costs on the order
 	$query = 'select';
 	$query .= ' orderproduct.op_qty as "quantity",';
 	$query .= ' attributevalue.attrval_price as "price",';
@@ -46,34 +47,37 @@ function getSubtotal($order_pk){
 	$query .= ' on orderproductattributevalues.opattr_attr_id = attribute.id';
 	$query .= ' inner join attributevalue';
 	$query .= ' on orderproductattributevalues.opattr_attrval_id = attributevalue.attrval_id';
-	$query .= ' where orderproduct.op_order_id = ' . $order_pk;
+	$query .= ' where orderproduct.op_order_id = ?';
 	
 	
-	$queryOutput = run_db_query($query); // Run query on DB and store result
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($order_pk));
+	
 	$subtotal = 0;
-
-	while ($row_order = $queryOutput ->fetch()){ // Iterate through each line from SQL query and total cost
+	while ($row_order = $statement ->fetch()){ // Iterate through each line from SQL query and total cost
 		$subtotal = $subtotal + $row_order[2];
 	}			
 	
 	return $subtotal; // Return total of all line costs
-
 }
+
 
 function getGST($cost_subtotal, $order_country){
 #Returns the GST amount required for payment
 
-	global $gstRate;
+	global $gstRate; // Value is defined in config.php
 
-	if ($order_country == "Australia"){
+	if ($order_country == "Australia"){ // If Australia, apply GST
 		return $cost_subtotal * $gstRate;
 	}
 
-	return 0;
-
+	return 0; // If not Australia, do not apply GST
 }
 
-function getItemDimensions($order){
+
+function getItemDimensions($order_pk){
+# Returns the item dimensions required for shipping the order
 
 	$query = ' select';
 	$query .= ' product.prod_name as "product",';
@@ -95,51 +99,64 @@ function getItemDimensions($order){
 	$query .= ' on orderproductattributevalues.opattr_attr_id = attribute.id';
 	$query .= ' inner join attributevalue';
 	$query .= ' on orderproductattributevalues.opattr_attrval_id = attributevalue.attrval_id';
-	$query .= ' where "Order".order_id = ' . $order;
+	$query .= ' where "Order".order_id = ?';
 	
-	$queryOutput = run_db_query($query); // Run query on DB and store result
-	return $queryOutput; // Return query results
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($order_pk));
 	
-	}
-	
-function updateOrderCosts($order_id, $cost_shipping, $cost_gst, $cost_subtotal, $cost_total){
-# Update the order fields in the DB
-
-	$query = 'update "Order" set ORDER_SHIPPINGAMOUNT = ' . $cost_shipping . ', ORDER_TAXAMOUNT = ' . $cost_gst;
-	$query .= ', ORDER_PRODUCTAMOUNT = ' .  $cost_subtotal . ', ORDER_TOTAL = ' . $cost_total . ' where order_id = ' . $order_id;
-	run_db_query($query);
+	return $statement; // Return query results
 }	
 
-function updatePaymentConfirmation($order_id, $confirmation_id) {
+	
+function updateOrderCosts($order_pk, $cost_shipping, $cost_gst, $cost_subtotal, $cost_total){
+# Update the order fields in the DB
+
+	$query = 'update "Order" set ORDER_SHIPPINGAMOUNT = ?, ORDER_TAXAMOUNT = ?, ORDER_PRODUCTAMOUNT = ?, ORDER_TOTAL = ? where order_id = ?';
+	
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($order_pk, $cost_shipping, $cost_gst, $cost_subtotal, $cost_total));
+}	
+
+
+function updatePaymentConfirmation($order_pk, $confirmation_id) {
 # Update the order with payment confirmation id. 
 	
-	$query = "update \"Order\" set ORDER_PAYMENT_CONFIRMATION_ID = '" . $confirmation_id ."'";
-	$query .= ' where order_id = ' . $order_id;
-	run_db_query($query);
+	$query = 'update "Order" set ORDER_PAYMENT_CONFIRMATION_ID = ? where order_id = ?';
+	
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($order_pk, $confirmation_id));
 }
 
 
-function getOrderTotal($order_id){
+function getOrderTotal($order_pk){
 # Returns the total order cost
 	
-	// Build query to return total order cost
-	$query = 'select';
-	$query .= ' order_total as "total"';
-	$query .= ' from "Order"';
-	$query .= ' where order_id = ' . $order_id;
+	$query = 'select order_total as "total" from "Order" where order_id = ?';
 	
-	$ordertotal=array(); //initialize
-	$queryOutput = run_db_query($query); // Run query
-	$ordertotal = $queryOutput->fetch(PDO::FETCH_ASSOC);
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($order_pk));
+	
+	$ordertotal = array(); //initialize array
+	$ordertotal = $statement->fetch(PDO::FETCH_ASSOC);
+	
 	return $ordertotal['total'];
 }
 
-function getAllCosts($order_id){
 
-	$query = 'select ORDER_SHIPPINGAMOUNT, ORDER_TAXAMOUNT, ORDER_PRODUCTAMOUNT, ORDER_TOTAL from "Order" where order_id = ' . $order_id;
-	$queryOutput = run_db_query($query); // Run query
-	return $queryOutput->fetch(); // Return first row as only a single row returned from query
+function getAllCosts($order_pk){
+# Returns all the cost values in an order
 
+	$query = 'select ORDER_SHIPPINGAMOUNT, ORDER_TAXAMOUNT, ORDER_PRODUCTAMOUNT, ORDER_TOTAL from "Order" where order_id = ?';
+	
+	$dbo = db_connect(); // Connect to DB and run query
+	$statement = $dbo->prepare($query);
+	$statement->execute(array($order_pk));	
+	
+	return $statement->fetch(); // Return first row as only a single row returned from query
 }
 
 ?>
